9e014046c00a8786f988cf8f68dfb262aa3d5ef0
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-01-22 15:24:50 -0500

    Style changes

--- app/schedule/schedule.controller.js
+++ app/schedule/schedule.controller.js
@@ -1,6 +1,11 @@
 angular.module('app').controller('ScheduleCtrl', function($scope, ScheduleFactory) {
-
   $scope.shortSchedule = ScheduleFactory.sendSchedule();
-
   $scope.navigateToTickeLink = ScheduleFactory.navigateToTickeLink;
+
+  $scope.isCollapsed = true;
+
+  $scope.toggleDetail = function() {
+    if ($scope.isCollapsed = true) $scope.isCollapsed = false;
+    else if ($scope.isCollapsed = false) $scope.isCollapsed = true;
+  }
 });
